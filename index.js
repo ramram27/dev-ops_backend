7e93448cbb5602ddf47d71cdbc5c5ed98c8e06d1
Rohit <rohitkumar@caplineservices.com>
2026-04-06 21:21:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', async(req , res) =>{
+    res.send(`
+        <div>
+        <h1>Student Info:</h1>
+        <h2>name: Rohit</h2>
+        <h2>Roll No: 12345</h2>
+        <h2>Branch: CSE</h2>
+        </div>
+        `)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
